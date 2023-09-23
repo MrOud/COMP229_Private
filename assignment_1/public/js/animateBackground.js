cc1e6324ddab6f8baf1251d1bd3ca5239f6651b7
Kris Oud <oudster@gmail.com>
2023-09-23 09:22:47 -0400

    added in backgrounds and navigation items

---
+++ assignment_1/public/js/animateBackground.js
@@ -0,0 +1,5 @@
+const stop1 = document.getElementById('gradient-stop1')
+const stop2 = document.getElementById('gradient-stop2')
+
+console.log(stop1, stop2)
+stop1.setAttributeNS(null, 'offset', '1%')
