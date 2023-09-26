74025a6600689ea0eb1d81c71c656362e27a22b4
Kris Oud <oudster@gmail.com>
2023-09-25 23:19:22 -0400

    removes unused session
    not a part of the assignment, removed

--- assignment_1/app/controllers/contact.server.controller.js
+++ assignment_1/app/controllers/contact.server.controller.js
@@ -1,9 +1,4 @@
 exports.render = function(req, res) {
-
-    if (req.session.lastVisit) {
-        console.log(req.session.lastVisit)
-    }
-
     res.render('Contact', {
         pageName: 'Contact'
     })
--- assignment_1/app/controllers/services.server.controller.js
+++ assignment_1/app/controllers/services.server.controller.js
@@ -1,9 +1,4 @@
 exports.render = function(req, res) {
-
-    if (req.session.lastVisit) {
-        console.log(req.session.lastVisit)
-    }
-
     res.render('services', {
         pageName: 'Services'
     })
